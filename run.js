af241097636c8767637e6a931a5da3b207847ebc
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-04-25 20:31:34 -0700

    Don't need html and csv reports for the moment, and will never not need to create json reports

--- run.js
+++ run.js
@@ -26,12 +26,9 @@ program
   .option('-s, --sites [sites]', 'a comma delimited list of site urls to analyze with Lighthouse', (str) => str.split(','), [])
   .option('-f, --file [path]', 'an input file with a site url per-line to analyze with Lighthouse')
   .option('-p, --params <params>', 'extra parameters to pass to lighthouse cli for each execution e.g. -p "--perf --quiet"')
-  .option('-h, --html', 'generate an html report alongside the json report')
-  .option('--csv', 'generate a csv report alongside the json report')
   .option('-o, --out [out]', `the output folder to place reports, defaults to '${execute.OUT}'`)
   .option('-g, --use-global', 'use a global lighthouse install instead of the dependency version')
   .option('-v, --verbose', 'enable verbose logging')
-  .option('--no-report', 'remove individual json reports for each site')
   .option('--print', 'print the final summary to stdout')
   .parse(process.argv)
 
