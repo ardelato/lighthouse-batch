73f72fe27aa832f445250f321961afac4c236479
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-06-01 15:44:38 -0700

    Accidently removed the verbose flag in the process

--- run.js
+++ run.js
@@ -9,6 +9,7 @@ const program = new Command();
 program
   .addOption(new Option('-s, --sites [sites...]', 'Space delimited list of site urls to analyze with Lighthouse', []))
   .addOption(new Option('-f, --file [path]', 'an input file with a site url per-line to analyze with Lighthouse'))
+  .addOption(new Option('-v, --verbose', 'enable verbose logging'))
   .addOption(new Option('-o, --out [out]', `the output folder to place reports, defaults to './report/lighthouse/'`))
   .addOption(new Option('-t, --times <number>', 'if specified, will run lighthouse on each url passed N times, otherwise will only run once',1))
   .addOption(new Option('-r, --role <string>','Type of worker to run as').choices(['standalone','publisher','consumer']).makeOptionMandatory(true))
