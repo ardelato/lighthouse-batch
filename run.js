d78ba9ab10d8e9c852500cfc04b7be037b789428
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-06-01 15:42:33 -0700

    Updating available command options Changing options to be Objects to allow for choices requirement Added a new option to declare the type of worker

--- run.js
+++ run.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 'use strict'
 
-import { Command } from 'commander/esm.mjs';
+import { Command, Option } from 'commander/esm.mjs';
 import execute from './index.js'
 
-const program = new Command;
+const program = new Command();
 
 program
-  .option('-s, --sites [sites...]', 'Space delimited list of site urls to analyze with Lighthouse', [])
-  .option('-f, --file [path]', 'an input file with a site url per-line to analyze with Lighthouse')
-  .option('-p, --params <params>', 'extra parameters to pass to lighthouse cli for each execution e.g. -p "--perf --quiet"')
-  .option('-o, --out [out]', `the output folder to place reports, defaults to './report/lighthouse/'`)
-  .option('-v, --verbose', 'enable verbose logging')
-  .option('-t, --times <number>', 'if specified, will run lighthouse on each url passed N times, otherwise will only run once',1)
-  .parse(program.argv)
+  .addOption(new Option('-s, --sites [sites...]', 'Space delimited list of site urls to analyze with Lighthouse', []))
+  .addOption(new Option('-f, --file [path]', 'an input file with a site url per-line to analyze with Lighthouse'))
+  .addOption(new Option('-o, --out [out]', `the output folder to place reports, defaults to './report/lighthouse/'`))
+  .addOption(new Option('-t, --times <number>', 'if specified, will run lighthouse on each url passed N times, otherwise will only run once',1))
+  .addOption(new Option('-r, --role <string>','Type of worker to run as').choices(['standalone','publisher','consumer']).makeOptionMandatory(true))
+
+program.parse()
 
 execute(program.opts())
