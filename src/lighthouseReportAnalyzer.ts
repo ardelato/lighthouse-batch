b0ce1b968ce10d7b4b39e1c3576c21ecc23740c3
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-04-30 10:06:25 -0700

    add methods to retrieve specific performance scores

--- src/lighthouseReportAnalyzer.ts
+++ src/lighthouseReportAnalyzer.ts
@@ -4,10 +4,66 @@ import { resolve } from "path";
 
 export class LighthouseAnalyzer {
   results: Result;
+  score: {
+    performance: number,
+
+  }
 
   constructor (file: string) {
     const filePath = resolve(file)
     const content = readFileSync(filePath, 'utf8')
     this.results = JSON.parse(content)
+    this.score = {
+      performance: this.getPerformanceScore()
+    }
   }
+
+  getURL() {
+    return this.results.requestedUrl
+  }
+
+  getFormFactor() {
+    return this.results.configSettings.formFactor
+  }
+
+  getScore() {
+
+  }
+
+  getPerformanceScore() {
+
+  }
+
+  // First Contentful Paint
+  getFCPTime() {
+    const timing = this.results.audits["first-contentful-paint"].numericValue ?? 0
+
+    return Math.floor(timing)
+  }
+
+  // Speed Index
+  getSIScore() {
+
+  }
+
+  // Largest Contentful Paint
+  getLCPScore() {
+
+  }
+
+  // Interactive
+  getTTIScore() {
+
+  }
+
+  // Total Blocking Time
+  getTBTScore() {
+
+  }
+
+  // Cumulative Layout Shift
+  getCLSScore() {
+
+  }
+
 }
