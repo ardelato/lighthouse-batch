6e9922a915c41288d50847905dee4f0aea1e6eb3
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-04-30 10:05:41 -0700

    create collection to keep track of site metrics

---
+++ src/siteMetrics.ts
@@ -0,0 +1,45 @@
+import SimplDB from 'simpl.db'
+
+const db = new SimplDB.Database({
+  collectionsFolder: './tmp/',
+  collectionTimestamps: true,
+  dataFile: '/tmp/backup.json',
+  tabSize: 2
+});
+
+export type SiteMetric = {
+  url: string,
+  formFactor: 'desktop' | 'mobile',
+  score: {}
+}
+
+export default class Sites {
+  private static sitesDB = db.createCollection<SiteMetric>('metrics')
+
+  public static createOrUpdate(site: SiteMetric) {
+    if (Sites.entryExists(site)) {
+      this.update(site);
+    } else {
+      this.create(site);
+    }
+  }
+
+  public static create(site: SiteMetric) {
+    this.sitesDB.create({ ...site })
+  }
+
+  public static update(site: SiteMetric) {
+    this.sitesDB.update(
+      s => s = site,
+      s => s.url === site.url && s.formFactor === site.formFactor
+    )
+  }
+
+  public static clean() {
+    this.sitesDB.remove()
+  }
+
+  private static entryExists(site: SiteMetric) {
+    return this.sitesDB.has(s => s.url === site.url && s.formFactor === site.formFactor)
+  }
+}
