e1c4021b1c42ad7abfc01ee09d11e31358b0cbdd
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-05-01 11:43:19 -0700

    update summary logic

--- src/summary.ts
+++ src/summary.ts
@@ -1,86 +1,115 @@
-import { readdirSync, readFileSync, appendFileSync } from 'fs';
+import { readdirSync } from 'fs';
+import * as util from 'util';
 import path from 'path';
+import {Logger} from "tslog"
+import { LighthouseAnalyzer } from './lighthouseReportAnalyzer';
+import SiteMetrics, { MappedSiteMetric, SiteMetric } from './siteMetrics';
 
-const TIMES_RAN = process.argv[2];
+const log = new Logger();
 
-function parseReports(files, dir){
-     const defaultsiteScore = {
-        "performance": 0,
-        "firstContentfulPaint": 0,
-        "firstMeaningfulPaint": 0,
-        "largestContentfulPaint": 0,
-        "firstCPUIdle": 0,
-        "interactive": 0,
-        "speedIndex": 0,
-        "estimatedInputLatency": 0,
-        "totalBlockingTime": 0,
-        "cumulativeLayoutShift": 0,
+function getAllReports() {
+    const files = readdirSync(path.resolve('./reports/'))
+
+    const reports: LighthouseAnalyzer[] = files.map(file => {
+        log.info(`Analyzing ${file}`)
+        return new LighthouseAnalyzer(`./reports/${file}`)
+    })
+
+    return reports
+}
+
+function getSiteMetrics(reports: LighthouseAnalyzer[]){
+    return reports.map(report => {
+        return {
+            url: report.getURL(),
+            formFactor: report.getFormFactor(),
+            score: report.getSummarizedScores(),
+        }
+    })
+}
+
+function rs(scores): MappedSiteMetric {
+    const reducedScores: MappedSiteMetric = new Map();
+
+    scores.forEach(score => {
+        const siteScore = reducedScores.get(score.url)
+        if (siteScore && siteScore[score.formFactor]) {
+            aggregatedScores(siteScore[score.formFactor], score.score)
+        } else if (siteScore) {
+            siteScore[score.formFactor] = new Map<string, number>()
+
+            aggregatedScores(siteScore[score.formFactor], score.score)
+        } else {
+            const newScore = {}
+            newScore[score.formFactor] = new Map<string, number>()
+
+            aggregatedScores(newScore[score.formFactor],score.score)
+            reducedScores.set(score.url, newScore)
+        }
+    })
+
+    return reducedScores
+}
+
+function aggregatedScores(previousScore: Map<string, number>, currentScore: Map<string, number>){
+    for (let [audit, score] of currentScore) {
+        const previousAuditScore = previousScore.get(audit) || 0
+        previousScore.set(audit, score + previousAuditScore)
     }
-    const sites = {};
-    const filteredFiles = files.filter(file => file && !file.match(/processedSites.txt/g) && !file.match(/failedSites.txt/g));
+}
 
-    for( const fileName of filteredFiles ){
-        console.log(fileName)
-        const file = readFileSync(path.join(dir,fileName));
-        const json = JSON.parse(file);
+function averageScores(scores: MappedSiteMetric, averageBy: number): MappedSiteMetric {
+    for (let [url, formFactorScores] of scores) {
+        const formFactors = Object.keys(formFactorScores)
+        formFactors.forEach(formFactor => {
+            averageScoresHelper(formFactorScores[formFactor],averageBy)
+        })
+    }
 
-        const formFactor = json.configSettings.formFactor
-        const metrics = json.audits.metrics.details.items[0]
-        const url = json.finalUrl
+    return scores
+}
 
-        const siteScore = {
-            "performance": json.categories.performance.score,
-            "firstContentfulPaint": metrics.firstContentfulPaint,
-            "firstMeaningfulPaint": metrics.firstMeaningfulPaint,
-            "largestContentfulPaint": metrics.largestContentfulPaint,
-            "firstCPUIdle": metrics.firstCPUIdle,
-            "interactive": metrics.interactive,
-            "speedIndex": metrics.speedIndex,
-            "estimatedInputLatency": metrics.estimatedInputLatency,
-            "totalBlockingTime": metrics.totalBlockingTime,
-            "cumulativeLayoutShift": metrics.cumulativeLayoutShift,
-        }
+function averageScoresHelper(scores: Map<string, number>,averageBy: number) {
+    for (let [audit, score] of scores) {
+        const newScore = parseFloat((score / averageBy).toFixed(2))
+        scores.set(audit, newScore)
+    }
+}
 
-        if(sites[url]){
-            sites[url][formFactor] = reduceScores(sites[url][formFactor],siteScore)
-        }
-        else{
-            sites[url] = {
-                desktop: formFactor === 'desktop' ? siteScore : defaultsiteScore,
-                mobile: formFactor === 'mobile' ? siteScore : defaultsiteScore
+function transformScoresToSiteMetric(scores: MappedSiteMetric): SiteMetric[] {
+    const transformedScores: SiteMetric[] = []
+
+    for (let [url, formFactorScores] of scores) {
+        const desktopEntries = formFactorScores['desktop']?.entries()
+        const mobileEntries = formFactorScores['mobile']?.entries()
+
+        const newSiteMetric = {
+            url: url,
+            scores: {
+                'desktop': desktopEntries ? Object.fromEntries(desktopEntries) : {},
+                'mobile': mobileEntries ? Object.fromEntries(mobileEntries) : {}
             }
         }
+
+        transformedScores.push(newSiteMetric)
     }
-    return sites
+
+    return transformedScores
 }
 
-function reduceScores(accumulator, current){
-    for( var audit of Object.keys(accumulator)){
-        accumulator[audit] += current[audit]
-    }
-    return accumulator
+function saveScores(scores) {
+    scores.forEach(score => {
+        SiteMetrics.createOrUpdate(score)
+    })
 }
-
-function averageScores(sites){
-    for( var site of Object.keys(sites)){
-        for( var formFactor of Object.keys(sites[site])){
-            for( var audit of Object.keys(sites[site][formFactor])){
-                sites[site][formFactor][audit] /= TIMES_RAN
-            }
-        }
-    }
-    return sites
+function printScores(scores) {
+    log.info(util.inspect(scores, false, 3))
 }
+const reports = getAllReports()
+const scores = getSiteMetrics(reports)
+const rsScores = rs(scores)
+const averagedScores = averageScores(rsScores, 1)
+const transformedScores = transformScoresToSiteMetric(averagedScores)
+printScores(transformedScores)
 
-function generateScoreSummary(branch){
-    const fileDir = './report/lighthouse';
-    const dir = path.join(fileDir, branch);
-    const branchFiles = readdirSync(dir);
-    const sumSiteScores = parseReports(branchFiles,dir);
-    const averageSiteScores = averageScores(sumSiteScores);
-    const summaryFile = path.join('.',`${branch}_scores.json`);
-    appendFileSync(summaryFile,JSON.stringify(averageSiteScores,null,2));
-}
-
-generateScoreSummary('master');
-generateScoreSummary('new');
+saveScores(transformedScores)
