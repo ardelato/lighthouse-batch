ffa939178479dcc243eb8cff7ae3fddcaa51411b
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-05-03 09:27:14 -0700

    remove lhscript

--- lhscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TIMES=5
-
-node run.js -t $TIMES -f ./master.txt --out "./report/lighthouse/master"
-
-node run.js -t $TIMES -f ./new.txt --out "./report/lighthouse/new"
-
-node summary.js $TIMES
