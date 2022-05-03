d85b26fe7b3617e642faa43e7e5d51b8a25995ef
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-05-03 10:21:11 -0700

    eslint: fix ab-batch.ts errors

--- src/commands/ab-batch.ts
+++ src/commands/ab-batch.ts
@@ -1,12 +1,12 @@
-import { Command, Flags } from '@oclif/core'
-import { executeABBatch } from "../batch-executor"
-import path from 'path';
+import {Command, Flags} from '@oclif/core'
+import {executeABBatch} from '../batch-executor'
+import path from 'node:path'
 
 export default class ABBatcher extends Command {
   static description = 'Run Lighthouse to compare results against a base site'
 
   static examples = [
-    `$ lh abBatch https://prod.google.com https://dev.google.com --pathsFile paths.txt --times 5`,
+    '$ lh abBatch https://prod.google.com https://dev.google.com --pathsFile paths.txt --times 5',
   ]
 
   static args = [
@@ -18,8 +18,8 @@ export default class ABBatcher extends Command {
     {
       name: 'comparisonURL',
       required: true,
-      description: 'The other URL to compare results with'
-    }
+      description: 'The other URL to compare results with',
+    },
   ]
 
   static flags = {
@@ -27,39 +27,39 @@ export default class ABBatcher extends Command {
       char: 'o',
       description: 'The output folder to place reports',
       default: `${path.resolve('./')}/reports`,
-      parse: async input => `${path.resolve('./')}/${input}`
+      parse: async input => `${path.resolve('./')}/${input}`,
     }),
     verbose: Flags.boolean({
       char: 'v',
       description: 'Enable Verbose logging',
-      default: false
+      default: false,
     }),
     times: Flags.integer({
       char: 't',
       description: 'Number of times to run Lighthouse on each url passed',
-      default: 1
+      default: 1,
     }),
     clean: Flags.boolean({
       description: 'Removes all processed sites in temp database, forcing a clean audit of all passed sites',
-      default: false
+      default: false,
     }),
     formFactor: Flags.string({
       description: 'Compare the sites with a specified form factor',
       default: 'both',
-      options: ['desktop', 'mobile', 'both']
+      options: ['desktop', 'mobile', 'both'],
     }),
     pathsFile: Flags.string({
-      description: 'A text file that contains additional paths to iterator through for both sites, if none specified, will just run on the urls passed'
+      description: 'A text file that contains additional paths to iterator through for both sites, if none specified, will just run on the urls passed',
     }),
-    help: Flags.help()
+    help: Flags.help(),
   }
 
   async catch(error) {
-    console.error(error.message);
+    console.error(error.message)
   }
 
   async run() {
-    const { args, flags } = await this.parse(ABBatcher);
+    const {args, flags} = await this.parse(ABBatcher)
     executeABBatch({...args, ...flags})
   }
 }
