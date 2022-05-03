85e363d57d9b55f99ec8d90913be1c5b58c69d97
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-05-03 10:24:38 -0700

    eslint: fix chrome-runner.ts errors

--- src/chrome-runner.ts
+++ src/chrome-runner.ts
@@ -1,18 +1,19 @@
-import { LaunchedChrome } from "chrome-launcher"
-import { killAll, launch } from 'chrome-launcher';
-import { Logger } from "tslog";
+import {LaunchedChrome} from 'chrome-launcher'
+import {launch} from 'chrome-launcher'
+import {Logger} from 'tslog'
 
 const log = new Logger({
-  minLevel: 'error'
-});
+  minLevel: 'error',
+})
 export default class ChromeRunner {
-  static chromeFlags = ["--disable-gpu"];
+  static chromeFlags = ['--disable-gpu'];
   chrome!: LaunchedChrome
 
-  constructor (headless: boolean = true, verbose: boolean = false) {
+  constructor(headless = true, verbose = false) {
     if (headless) {
-      ChromeRunner.chromeFlags.push("--headless");
+      ChromeRunner.chromeFlags.push('--headless')
     }
+
     if (verbose) {
       log.setSettings({
         minLevel: 'info',
@@ -23,13 +24,13 @@ export default class ChromeRunner {
   async start(): Promise<number> {
     log.info('Starting ChromeRunner')
     this.chrome = await launch({
-      chromeFlags: ChromeRunner.chromeFlags
+      chromeFlags: ChromeRunner.chromeFlags,
     })
-    return this.chrome.port;
+    return this.chrome.port
   }
 
   async stop(): Promise<void> {
     log.info('Stopping ChromeRunner')
-    await this.chrome.kill();
+    await this.chrome.kill()
   }
 }
