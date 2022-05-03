0a737a8da7d1e8511318abf35c1c7ec1c80d4b89
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-05-03 10:20:37 -0700

    eslint: fix batch.ts errors

--- src/commands/batch.ts
+++ src/commands/batch.ts
@@ -1,20 +1,21 @@
-import { Command, Flags } from '@oclif/core'
-import path from 'path';
-import { executeBatch } from '../batch-executor';
+import {Command, Flags} from '@oclif/core'
+import path from 'node:path'
+import {executeBatch} from '../batch-executor'
 
 export default class Batch extends Command {
   static description = 'Run Lighthouse on all passed sites for a given number of times'
 
   static examples = [
-    `$ lh batch --file <file_name.txt> --times 5`,
-    `$ lh batch --sites https://example.com https://google.com --times 5 --clean`,
+    '$ lh batch --file <file_name.txt> --times 5',
+    '$ lh batch --sites https://example.com https://google.com --times 5 --clean',
   ]
+
   static flags = {
     sites: Flags.string({
       char: 's',
       description: 'Space delimited list of site urls to analyze with Lighthouse',
       multiple: true,
-      exclusive: ["file"]
+      exclusive: ['file'],
     }),
     file: Flags.string({
       char: 'f',
@@ -24,39 +25,40 @@ export default class Batch extends Command {
       char: 'o',
       description: 'The output folder to place reports',
       default: `${path.resolve('./')}/reports`,
-      parse: async input => `${path.resolve('./')}/${input}`
+      parse: async input => `${path.resolve('./')}/${input}`,
     }),
     verbose: Flags.boolean({
       char: 'v',
       description: 'Enable Verbose logging',
-      default: false
+      default: false,
     }),
     times: Flags.integer({
       char: 't',
       description: 'Number of times to run Lighthouse on each url passed',
-      default: 1
+      default: 1,
     }),
     formFactor: Flags.string({
       description: 'Compare the sites with a specified form factor',
       default: 'desktop',
-      options: ['desktop', 'mobile']
+      options: ['desktop', 'mobile'],
     }),
     clean: Flags.boolean({
       description: 'Removes all processed sites in temp database, forcing a clean audit of all passed sites',
-      default: false
+      default: false,
     }),
-    help: Flags.help()
+    help: Flags.help(),
   }
 
   async catch(error) {
-    console.error(error.message);
+    console.error(error.message)
   }
 
   async run() {
-    const { flags } = await this.parse(Batch);
+    const {flags} = await this.parse(Batch)
     if (!flags.sites && !flags.file) {
-      this.error('Error: sites or file must be specified');
+      this.error('Error: sites or file must be specified')
     }
+
     executeBatch(flags)
   }
 }
