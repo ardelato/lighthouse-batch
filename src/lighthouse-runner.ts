94ef74e357686cf52d7eb5897f0acdb0de8ca506
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-05-03 10:30:36 -0700

    eslint: fix lighthouse-runner.ts errors

--- src/lighthouse-runner.ts
+++ src/lighthouse-runner.ts
@@ -1,44 +1,48 @@
-import lighthouse from "lighthouse";
-import { existsSync, writeFileSync } from 'fs';
-import { resolve } from 'path';
-import { exec } from 'child_process';
-import { Logger } from "tslog";
+import lighthouse from 'lighthouse'
+import {existsSync, writeFileSync} from 'node:fs'
+import {resolve} from 'node:path'
+import {exec} from 'node:child_process'
+import {Logger} from 'tslog'
 
 const log = new Logger({
-  minLevel: 'error'
-});
+  minLevel: 'error',
+})
 
 export default class LightHouseRunner {
-  private static runID: number = 0;
+  private static runID = 0;
   static outputDir: string;
   options = {
     logLevel: 'error',
     output: 'json',
+    port: 0,
   };
+
   configPath: string
   url: string;
   outputFileName: string;
   fullPathOutputFileName: string;
 
-  constructor (outputDir: string, url: string, port: number, formFactor: 'desktop' | 'mobile', verbose = false) {
+  constructor(outputDir: string, url: string, port: number, formFactor: 'desktop' | 'mobile', verbose = false) {
     if (!LightHouseRunner.outputDir) {
       LightHouseRunner.outputDir = outputDir
     }
-    if(!this.reportDirectoryExists() ) {
-      this.makeReportDirectory();
+
+    if (!this.reportDirectoryExists()) {
+      this.makeReportDirectory()
     }
+
     if (verbose) {
       this.options.logLevel = 'info'
       log.setSettings({
-        minLevel: 'info'
+        minLevel: 'info',
       })
     }
 
-    this.configPath = this.getConfigPath(formFactor);
+    this.configPath = this.getConfigPath(formFactor)
     this.url = url
-    this.options["port"] = port;
+    this.options.port = port
 
-    this.outputFileName = this.getSiteName(url);
+    this.outputFileName = this.getSiteName(url)
 
     LightHouseRunner.runID += 1
 
@@ -51,10 +55,10 @@ export default class LightHouseRunner {
       const results = await lighthouse(this.url, this.options, require(this.configPath))
 
       log.info(`Writing Results to ${this.fullPathOutputFileName}`)
-      writeFileSync(this.fullPathOutputFileName, results.report);
-    } catch (e) {
-      log.error(new Error(`Failed to Run Lighthouse on ${this.url} \n${e}`))
-      throw e
+      writeFileSync(this.fullPathOutputFileName, results.report)
+    } catch (error) {
+      log.error(new Error(`Failed to Run Lighthouse on ${this.url} \n${error}`))
+      throw error
     }
   }
 
@@ -64,9 +68,9 @@ export default class LightHouseRunner {
 
   private makeReportDirectory() {
     try {
-        exec(`mkdir -p ${LightHouseRunner.outputDir}`)
-    } catch(e: any) {
-      throw new Error(e);
+      exec(`mkdir -p ${LightHouseRunner.outputDir}`)
+    } catch (error: any) {
+      throw new Error(error)
     }
   }
 
@@ -74,11 +78,11 @@ export default class LightHouseRunner {
     return existsSync(LightHouseRunner.outputDir)
   }
 
-  private getSiteName(url: string): string{
-    return url.replace(/(?:^https?:\/\/)(?:www\.)?(?<domain>[a-z0-9\-\.]+)(?:\.[a-z\.]+)(?<path>[\/]?.*)/,'$<domain>$<path>').replace(/[\/\?#:\*\$@\!\.\+]/g, '-')
+  private getSiteName(url: string): string {
+    return url.replace(/^ht{2}ps?:\/{2}(?:w{3}\.)?(?<domain>[\d.a-z\-]+)\.[.a-z]+(?<path>\/?.*)/, '$<domain>$<path>').replace(/[!#$*+./:?@]/g, '-')
   }
 
   private getConfigPath(formFactor: 'desktop' | 'mobile'): string {
     return resolve(`./config/lr-${formFactor}-config.js`)
-  };
+  }
 }
